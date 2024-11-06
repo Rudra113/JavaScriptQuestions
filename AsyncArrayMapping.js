ee10e432a8d6498b899c51061c36a486d1cf5057
Rudra Ranjan Mohant <rudraranjanmohanty@gmail.com>
2024-11-06 15:49:20 +0530

    Array mapping problem

--- AsyncArrayMapping.js
+++ AsyncArrayMapping.js
@@ -1,18 +1,18 @@
 async function delayMultiply(numbers) {
-    const results = []
+    const results = [];
     for (const number of numbers) {
         const result = await new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(number * 2)
-            }, 500);
+            }, 500);;
         })
-        results.push(result)
+        results.push(result);
     }
-    return results
+    return results;
 }
 
 const numbers = [2, 5, 9, 8, 7]
 delayMultiply(numbers).then(results => {
-    console.log(results)
+    console.log(results);
 })
 
